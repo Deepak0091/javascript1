52f42c79640ab300883406ffc4d38f75da15add9
Deepak0091 <140441442+Deepak0091@users.noreply.github.com>
2023-08-01 10:33:44 +0000

    js series

---
+++ text.js
@@ -0,0 +1,29 @@
+// for (let i=1; i<= 10; i++) {
+//     var element = i;
+//     console.log(`outer loop value ${i}`);
+//     for (let j = 1 ; j <= 10 ;j ++) {
+//         console.log(i+'*'+j+'='+i*j)
+        
+//     }
+
+// }
+// let myarray=['tannu','annu','shivni']
+// for (let index = 0; index< myarray.length; index++) {
+//     const element = myarray[index];
+    
+// }
+// for (let index =0; index <= 20; index++){
+//                  if(index==5){
+//                     console.log('user value is five')
+//                     break;
+
+
+//                  }  
+//                       console.log(`value of i is ${index}`);
+
+// }
+let myarrey=["flah man"]
+
+for(const array of myarrey){
+    console.log(`Each char  is  ${array}`)
+}
